007be3d9e7e798eddabc85e2b3ae8bcf47152e80
Richard Campbell <betam4x@gmail.com>
2013-11-06 22:04:00 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeedFuExample::Application.config.secret_key_base = '6a9754288cb0b7fdda839cb7edfd0767440e83bbb385438a613af55d32f3cbb2ab2c1968a0c1235ee0a7ad752269fbfa4b9d2de5b05b94f46a7dea90a1a40b40'
---
+++ db/fixtures/development/products.rb
@@ -0,0 +1,7 @@
+Product.seed(:name, 
+  { name: "Banana", price: 0.29 },
+  { name: "Apple", price: 0.49 },
+  { name: "Strawberry", price: 0.10 },
+  { name: "Bag of Blueberries", price: 1.99 },
+  { name: "Pear", price: 0.75 },
+)
